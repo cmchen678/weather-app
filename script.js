451246527e66fea7156f38a215ea924b92976fb8
christian <cmchen678@gmail.com>
2026-08-13 15:57:31 -0400

    display current weather conditions in DOM

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ searchForm.addEventListener("submit", (event) => {
   const formData = new FormData(event.target);
   const location = formData.get("location-search");
 
-  getWeatherData(location).then((data) => console.log(data));
+  getWeatherData(location).then((data) => displayWeatherData(data));
 });
 
 const getWeatherData = async (location) => {
@@ -21,6 +21,8 @@ const getWeatherData = async (location) => {
     }
 
     const weatherData = await response.json();
+    const currentHour = new Date().getHours();
+    console.log(weatherData);
 
     const processedWeatherData = await {
       location: weatherData.resolvedAddress,
@@ -31,6 +33,40 @@ const getWeatherData = async (location) => {
       wind: weatherData.currentConditions.windspeed,
       condition: weatherData.currentConditions.conditions,
       icon: weatherData.currentConditions.icon,
+      hours: [
+        {
+          icon: weatherData.days[0].hours[currentHour + 1].icon,
+          temp: weatherData.days[0].hours[currentHour + 1].temp,
+        },
+        {
+          icon: weatherData.days[0].hours[currentHour + 2].icon,
+          temp: weatherData.days[0].hours[currentHour + 2].temp,
+        },
+        {
+          icon: weatherData.days[0].hours[currentHour + 3].icon,
+          temp: weatherData.days[0].hours[currentHour + 3].temp,
+        },
+        {
+          icon: weatherData.days[0].hours[currentHour + 4].icon,
+          temp: weatherData.days[0].hours[currentHour + 4].temp,
+        },
+        {
+          icon: weatherData.days[0].hours[currentHour + 5].icon,
+          temp: weatherData.days[0].hours[currentHour + 5].temp,
+        },
+        {
+          icon: weatherData.days[0].hours[currentHour + 6].icon,
+          temp: weatherData.days[0].hours[currentHour + 6].temp,
+        },
+        {
+          icon: weatherData.days[0].hours[currentHour + 7].icon,
+          temp: weatherData.days[0].hours[currentHour + 7].temp,
+        },
+        {
+          icon: weatherData.days[0].hours[currentHour + 8].icon,
+          temp: weatherData.days[0].hours[currentHour + 8].temp,
+        },
+      ],
       days: [
         {
           date: weatherData.days[1].datetime,
@@ -82,4 +118,61 @@ const getWeatherData = async (location) => {
   }
 };
 
-const displayWeatherData = (data) => {};
+const displayWeatherData = (data) => {
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+
+  const currentWeatherContainer = document.createElement("div");
+  currentWeatherContainer.classList.add("current-weather-container");
+
+  const description = document.createElement("p");
+  description.classList.add("description");
+  description.textContent = data.description;
+
+  const temp = document.createElement("p");
+  temp.classList.add("current-temp");
+  temp.textContent = `${Math.round(data.temp)}°`;
+
+  const condition = document.createElement("p");
+  condition.classList.add("current-condition");
+  condition.textContent = data.condition;
+
+  const location = document.createElement("p");
+  location.classList.add("location");
+  location.textContent = data.location;
+
+  const miscInfoContainer = document.createElement("ul");
+  miscInfoContainer.classList.add("misc-info-container");
+
+  const precip = document.createElement("li");
+  const precipLabel = document.createElement("p");
+  precipLabel.textContent = "Precipitation";
+  const precipProb = document.createElement("p");
+  precipProb.textContent = `${Math.round(data.precipprob)}%`;
+  precip.append(precipLabel, precipProb);
+
+  const humidity = document.createElement("li");
+  const humidityLabel = document.createElement("p");
+  humidityLabel.textContent = "Humidity";
+  const humidityPercent = document.createElement("p");
+  humidityPercent.textContent = `${Math.round(data.humidity)}%`;
+  humidity.append(humidityLabel, humidityPercent);
+
+  const wind = document.createElement("li");
+  const windLabel = document.createElement("p");
+  windLabel.textContent = "Wind";
+  const windSpeed = document.createElement("p");
+  windSpeed.textContent = `${data.wind} mph`;
+  wind.append(windLabel, windSpeed);
+
+  miscInfoContainer.append(precip, humidity, wind);
+
+  currentWeatherContainer.append(
+    description,
+    temp,
+    condition,
+    location,
+    miscInfoContainer,
+  );
+  container.append(currentWeatherContainer);
+};
